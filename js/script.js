0a411f1c4b999543481d0c93fe6404f8f6e130dc
guthomas <guillaumethomas42@gmail.com>
2017-03-10 17:15:57 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,5 +6,5 @@ xmlhttp.onreadystatechange = function() {
         document.getElementById("demo").innerHTML = jsonPretty;
     }
 };
-xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
+xmlhttp.open("GET", "http://guthomas/bistcoin", true);
 xmlhttp.send();
